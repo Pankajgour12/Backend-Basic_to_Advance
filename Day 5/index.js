93c18c57fb6da2edfa16f8eb1d0cfe9a490f621e
pankajgour12 <pankajgour5000@gmail.com>
2025-10-12 20:36:29 +0530

    Implement cryptographic functions for random bytes generation and SHA-256 hashing

--- Day 5/index.js
+++ Day 5/index.js
@@ -1,3 +1,27 @@
 
 
 
+const crypto = require('crypto');
+
+// 1. randomBytes 
+const randomValues = crypto.randomBytes(100)
+
+console.log(randomValues.toString('hex'));
+
+
+// 2. createHash
+
+const hashvalue = crypto.createHash('sha256').update('pankaj').digest('hex')
+
+const inputValue = 'pankaj'
+
+const matchValue = crypto.createHash('sha256').update(inputValue).digest('hex')
+
+
+if(hashvalue === matchValue){
+    console.log("You Can Login")
+}
+else{
+    console.log("Something Went Wrong")
+}
+
