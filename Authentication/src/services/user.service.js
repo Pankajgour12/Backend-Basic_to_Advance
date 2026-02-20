3796d2e20b482d7ef8ef1fac9e156b8b68c55dde
pankajgour12 <pankajgour5000@gmail.com>
2026-02-20 08:46:14 +0530

    test register api

--- Authentication/src/services/user.service.js
+++ Authentication/src/services/user.service.js
@@ -1,3 +1,12 @@
+import bcrypt from "bcrypt";
+import { User } from "../models/user.model.js";
+
 export const registerUser = async(username,password)=>{
-    console.log(username,password);
+    const hashedPassword = await bcrypt.hash(password,10)
+    const user = new User({username,password:hashedPassword})
+    return await user.save() 
+
+    
+
+
 }
